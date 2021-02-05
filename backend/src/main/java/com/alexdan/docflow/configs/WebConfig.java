ce57f1f8db11786e5b5729f3f84e4a61a9af6bff
Alex Dan <dan-aleksei2012@yandex.ru>
2021-02-05 16:24:42 +0300

    Added uploading of static files to the server and distributing them

--- backend/src/main/java/com/alexdan/docflow/configs/WebConfig.java
+++ backend/src/main/java/com/alexdan/docflow/configs/WebConfig.java
@@ -1,21 +1,34 @@
 package com.alexdan.docflow.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.server.ConfigurableWebServerFactory;
 import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.nio.file.Path;
+
 @Configuration
 class WebConfig implements WebMvcConfigurer {
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
 
         registry.addViewController("/login");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("files/**")
+                .addResourceLocations("file://" + uploadPath + "/");
     }
 
     @Bean
