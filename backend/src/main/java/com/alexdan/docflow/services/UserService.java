1ae4ddfdf5427db446c1bb1c800b7d972825e054
Alex Dan <dan-aleksei2012@yandex.ru>
2021-02-06 15:21:36 +0300

    Redesigned UserController: methods for interacting with databases moved in UserService.

--- backend/src/main/java/com/alexdan/docflow/services/UserService.java
+++ backend/src/main/java/com/alexdan/docflow/services/UserService.java
@@ -1,9 +1,11 @@
 package com.alexdan.docflow.services;
 
 import com.alexdan.docflow.data.DepartmentRepository;
+import com.alexdan.docflow.data.DocumentRepository;
 import com.alexdan.docflow.data.UserRepository;
 import com.alexdan.docflow.exceptions.UserNotFoundException;
 import com.alexdan.docflow.models.Department;
+import com.alexdan.docflow.models.Document;
 import com.alexdan.docflow.models.Role;
 import com.alexdan.docflow.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,18 +15,26 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
+import java.util.List;
 
 @Service
 public class UserService implements UserDetailsService {
 
-    @Autowired
-    private UserRepository userRepository;
 
-    @Autowired
-    private DepartmentRepository departmentRepository;
+    private final UserRepository userRepository;
+    private final DepartmentRepository departmentRepository;
+    private final DocumentRepository documentRepository;
 
     @Autowired
     BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, DepartmentRepository departmentRepository,
+                       DocumentRepository documentRepository) {
+        this.userRepository = userRepository;
+        this.departmentRepository = departmentRepository;
+        this.documentRepository = documentRepository;
+    }
 
     @Override
     public User loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -33,6 +43,17 @@ public class UserService implements UserDetailsService {
         if (user != null)
             return user;
         throw new UsernameNotFoundException("User " + username + " not found");
+    }
+
+    public User getUser(long id){
+
+        return userRepository.findById(id).
+                orElseThrow(()-> new UserNotFoundException(id));
+    }
+
+    public List<User> getAllUsers(){
+
+        return (List<User>) userRepository.findAll();
     }
 
     public User updateUser(User user){
@@ -72,6 +93,21 @@ public class UserService implements UserDetailsService {
         department.addEmployee(savedUser);
         departmentRepository.save(department);
         return savedUser;
+    }
+
+    public List<Document> getUsersDocuments(long id){
+
+        return this.getUser(id).getDocuments();
+    }
+
+    public Document addFile(long id, Document document){
+        User user = userRepository.findById(id).
+                orElseThrow(()-> new UserNotFoundException(id));
+        document.setUser(user);
+        Document savedDocument = documentRepository.save(document);
+        user.addDocument(savedDocument );
+        userRepository.save(user);
+        return savedDocument ;
     }
 
 }
